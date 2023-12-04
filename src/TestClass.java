cc363f68a76edda8601530e0ee24756ef193656c
anton <a.chernenko@aimprosoft.com>
2023-12-04 14:14:09 +0200

    br3

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
         System.out.println("Hello");
         System.out.println("Hello2 World");
         System.out.println("Hello2 World2");
-        System.out.println("Hello2 World3");
+        System.out.println("Hello2 Test World3");
+        System.out.println("Hello333 World3");
     }
 }
